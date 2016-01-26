755481746bfffb272eb56ed2d84b1414fbd514f3
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-01-26 13:28:38 -0500

    Added a class exercise, calculating the average value in an array of numbers

---
+++ week3/average-of-numbers.c
@@ -0,0 +1,41 @@
+/*
+ * Read in 'n' numbers from the user
+ * into an array and then calculate
+ * and print out the average.
+ *
+ * Michael Burrell
+ */
+
+#include <stdio.h>
+
+int
+main(void)
+{
+	int n;
+	printf("How many numbers are you going to enter?\n");
+	if (scanf("%d", &n) != 1) {
+		fprintf(stderr, "That was bad input\n");
+		return 1;
+	}
+
+	double x[n];
+	for (int i = 0; i < n; i++) {
+		if (scanf("%lf", &x[i]) != 1) {	// &x[i] is the same as x + i
+			fprintf(stderr, "That was bad input\n");
+			return 1;
+		}
+	}
+
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += x[i];
+	}
+	printf("The average is %lf\n", sum / n);
+	return 0;
+}
+
+
+
+
+
+
