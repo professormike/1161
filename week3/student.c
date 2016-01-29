027dbaf4c958971fa9a5f19d0929970ac25b593f
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-01-29 12:59:08 -0500

    Changed the student example to use structs

--- week3/student.c
+++ week3/student.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+struct student {
+	char name[20];
+	int student_no;
+};
+
 int
 main(void)
 {
-	char names[3][20];
-	for (int i = 0; i < sizeof names / sizeof *names; i++) {
-		printf("Enter name %d\n", i);
-		fgets(names[i], sizeof names[i] / sizeof *names[i], stdin);
+	struct student s[3];
+	for (int i = 0; i < sizeof s / sizeof *s; i++) {
+		printf("Enter name %d\n", i + 1);
+		fgets(s[i].name, sizeof s[i].name / sizeof *s[i].name, stdin);
+		printf("Enter student number %d\n", i + 1);
+		scanf("%d", &s[i].student_no);
+		getchar();	// read in and ignore newline character
 	}
-	for (int i = 0; i < sizeof names / sizeof *names; i++) {
-		printf("%s\n", names[i]);
+	for (int i = 0; i < sizeof s / sizeof *s; i++) {
+		printf("%d %s\n", s[i].student_no, s[i].name);
 	}
 	return 0;
 }
