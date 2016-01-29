b8acec60469f5e605b04ded847229251a26a0aef
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-01-29 11:03:06 -0500

    Updated all-positive.c to use polymorphism. To do this, we need the use of generic pointers (void *) and the use of a pointer-to-function (in this case, the is_positive pointer

--- week3/all-positive.c
+++ week3/all-positive.c
@@ -2,16 +2,19 @@
 
 static
 int
-all_positive(int *array, size_t n)
+all_positive(void *array, size_t n, size_t sz, int (*is_positive)(void *))
 /*
  * 'array' is a pointer to the first element of an array
- * of 'n' integers.
+ * of 'n' elements. Each element is 'sz' bytes in size. The
+ * function 'is_positive' can tell us whether any given
+ * element is positive.
  * Returns 0 if at least one element is negative (or 0)
  * Returns 1 if all elements are positive
  */
 {
 	for (size_t i = 0; i < n; i++) {
-		if (array[i] <= 0) {
+		void *elem_i_location = array + sz * i;
+		if (!(*is_positive)(elem_i_location)) {
 			return 0;
 		}
 	}
@@ -20,29 +23,18 @@ all_positive(int *array, size_t n)
 
 static
 int
-all_positive2(double *array, size_t n)
-/*
- * 'array' is a pointer to the first element of an array
- * of 'n' doubles.
- * Returns 0 if at least one element is negative (or 0)
- * Returns 1 if all elements are positive
- */
+is_positive_double(void *ptr)
 {
-	for (size_t i = 0; i < n; i++) {
-		if (array[i] <= 0) {
-			return 0;
-		}
-	}
-	return 1;
+	double *dbl_ptr = ptr;
+	return *dbl_ptr >= 0;
 }
-
 
 int
 main(void)
 {
 	double x[] = { 5, 6, 9, -3, 4, 2 };
 	printf("all numbers are positive? %s\n",
-		all_positive2(x, sizeof x / sizeof *x) ? "yes" : "no");
+		all_positive(x, sizeof x / sizeof *x, sizeof *x, is_positive_double) ? "yes" : "no");
 	return 0;
 }
 
