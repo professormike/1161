328590eb9e920d6d20d72667bf4b8a42e8e02075
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-02-02 14:37:09 -0500

    Added an example program which deliberately has a memory leak in it

---
+++ week4/memory-leak.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>	// for malloc
+#include <string.h>	// for strlen
+
+char *
+read_name(void)
+{
+	char *name = malloc(100);
+	fgets(name, 100, stdin);
+	return name;
+}
+
+int
+main(void)
+{
+	char *n;
+	do {
+		printf("Please enter your name: ");
+		n = read_name();
+		printf("Your name is %s\n", n);
+	} while (strlen(n) > 1);
+	return 0;
+}
