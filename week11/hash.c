4ffb632d9f27dd71015f10408187d557b707a5a9
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-04-01 13:10:31 -0400

    Initial implementation of a hash table (no collision detection yet)

--- week11/hash.c
+++ week11/hash.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+struct student {
+	unsigned int name_hash;
+	double grade;
+};
+
 unsigned int
 hash_string(char const *x)
 // courtesy of Clifton
@@ -16,12 +21,51 @@ hash_string(char const *x)
 	return s;
 }
 
+void
+insert_student(struct student *table, size_t table_size,
+char const *student_name, double student_grade)
+{
+	unsigned int hash = hash_string(student_name);
+	size_t position = hash % table_size;
+	table[position].name_hash = hash;
+	table[position].grade = student_grade;
+}
+
+double
+find_student(struct student const *table, size_t table_size,
+char const *student_name)
+{
+	unsigned int hash = hash_string(student_name);
+	size_t position = hash % table_size;
+	if (table[position].name_hash == hash) {
+		return table[position].grade;
+	} else {
+		return -1;	// signal that the grade wasn't found
+	}
+}
+
 int
 main(void)
 {
-	char line[100];
-	while (1) {
-		fgets(line, sizeof line, stdin);
-		printf("%u\n", hash_string(line));
-	}
+	struct student hash_table[11];
+#define ADD(nm, gr) insert_student(hash_table,	\
+			sizeof hash_table / sizeof *hash_table, nm, gr)
+#define GET(nm)	find_student(hash_table,	\
+			sizeof hash_table / sizeof *hash_table, nm)
+	ADD("Joe Smith", 80);
+	ADD("Jane Jones", 75);
+	ADD("Fred Flintstone", 40);
+	ADD("Wilma Flintstone", 90);
+	printf("Joe's mark: %lf\n", GET("Joe Smith"));
+	printf("Jane's mark: %lf\n", GET("Jane Jones"));
+	printf("Fred's mark: %lf\n", GET("Fred Flintstone"));
+	printf("Wilma's mark: %lf\n", GET("Wilma Flintstone"));
+	printf("Gary's mark: %lf\n", GET("Gary"));
+	return 0;
 }
+
+
+
+
+
+
