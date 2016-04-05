5d6163d219d93fac3055793a0dc3b1477598816b
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-04-05 13:43:55 -0400

    Implemented linear probing
    Created a file for chaining, but did not write any code for it yet

--- week12/hashtable/hash.c
+++ week12/hashtable/hash.c
@@ -1,14 +1,20 @@
+/*
+ * Implementation of a hash table with linear probing
+ */
+
 #include "hash.h"
 #include <stdio.h>
 
 struct student {
+	int taken;	// 1 if this element is taken; 0 if it's empty
 	unsigned int name_hash;
 	double grade;
 };
 
 #define TABLE_SIZE	5
 
-static struct student table[TABLE_SIZE];
+// = { 0 } initializes the whole table to be empty
+static struct student table[TABLE_SIZE] = { 0 };
 
 static
 unsigned int
@@ -32,8 +38,15 @@ insert_student(char const *student_name, double student_grade)
 {
 	unsigned int hash = hash_string(student_name);
 	size_t position = hash % TABLE_SIZE;
-	table[position].name_hash = hash;
-	table[position].grade = student_grade;
+	for (size_t i = 0; i < TABLE_SIZE; i++) {
+		size_t p = (position + i) % TABLE_SIZE;
+		if (!table[p].taken) {
+			table[p].name_hash = hash;
+			table[p].grade = student_grade;
+			table[p].taken = 1;
+			return;
+		}
+	}
 }
 
 double
@@ -41,10 +54,12 @@ find_student(char const *student_name)
 {
 	unsigned int hash = hash_string(student_name);
 	size_t position = hash % TABLE_SIZE;
-	if (table[position].name_hash == hash) {
-		return table[position].grade;
-	} else {
-		return -1;	// signal that the grade wasn't found
-	}
+	for (size_t i = 0; i < TABLE_SIZE; i++) {
+		size_t p = (position + i) % TABLE_SIZE;
+		if (table[p].name_hash == hash) {
+			return table[p].grade;
+		}
+	}	
+	return -1;	// signal that the grade wasn't found
 }
 
